3b67999ee5fd6095ccb8f19c73615d89198fc649
mr.chen <chenhui315503490@126.com>
2017-12-08 14:05:00 +0800

    1

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,9 +4,23 @@ const app = getApp()
 
 Page({
   data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000
   },
   onLoad: function () {
 
+  },
+
+  linkDetails: function () {
+    wx.navigateTo({
+      url: '/pages/index/details/details',
+    });
   }
 })
-  
