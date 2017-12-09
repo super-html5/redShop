25d33dfe18092ef9776137eea7bf5a243131035d
mr.chen <chenhui315503490@126.com>
2017-12-09 23:51:04 +0800

    详情页基本完成

--- pages/index/details/details.js
+++ pages/index/details/details.js
@@ -1,66 +1,60 @@
 // pages/index/details/details.js
+var sliderWidth = 160;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    tabs: ["图文详情", "产品参数"],
+    activeIndex: 1,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    this.setImageData();
+    this.setSwiperData();
+  },
+  /**
+   * 轮播图配置参数
+   */
+  setSwiperData: function () {
+    let indicatorDots = true;
+    let autoplay = true;
+    let interval = 5000;
+    let duration = 1000;
+    let _swiperData = {
+      'indicatorDots': indicatorDots,
+      'autoplay': autoplay,
+      'interval': interval,
+      'duration': duration
+    };
+    this.setData({
+      swiperData: _swiperData
+    });
+  },
+  /**
+   * 轮播图,图片配置
+   */
+  setImageData: function () {
+    let _imgUrls = [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ];
+    this.setData({
+      imgUrls: _imgUrls
+    });
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 切换navbar
    */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  tabClick: function (e) {
+    this.setData({
+      activeIndex: e.currentTarget.id
+    });
   }
 })
