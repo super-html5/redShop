84638993b619e7a5b4b2120826a3d8c575e134e0
mr.chen <chenhui315503490@126.com>
2017-12-11 16:12:13 +0800

    订单确认页面完成

--- pages/index/confirm/confirm.js
+++ pages/index/confirm/confirm.js
@@ -5,62 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    shoppingNumber: 1
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 加减方法
    */
-  onReady: function () {
-  
+  jzNum: function (e) {
+    console.log(e.currentTarget.dataset.status);
+    if (e.currentTarget.dataset.status == 'subtraction') {
+      if (this.data.shoppingNumber == 1) {
+        return;
+      }
+      this.setData({
+        shoppingNumber: this.data.shoppingNumber - 1
+      });
+    } else if (e.currentTarget.dataset.status == 'addition') {
+      this.setData({
+        shoppingNumber: this.data.shoppingNumber + 1
+      });
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
