e4a3ee4de7d9e387a0c909428524be37712cba65
mr.chen <chenhui315503490@126.com>
2017-12-11 17:05:17 +0800

    订单确认页面完成

--- pages/index/confirm/confirm.js
+++ pages/index/confirm/confirm.js
@@ -1,20 +1,19 @@
 // pages/index/confirm/confirm.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    shoppingNumber: 1
+    shoppingNumber: 1,
+    isSendUrl: '../../../img/unsuccess.png',
+    isSendClick: false,
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
 
   },
-
   /**
    * 加减方法
    */
@@ -33,4 +32,22 @@ Page({
       });
     }
   },
+
+  /**
+   * 是否自提
+   */
+  isSend: function () {
+    if (this.data.isSendClick == false) {
+      this.setData({
+        isSendUrl: '../../../img/success.png',
+        isSendClick: true
+      });
+    }else{
+      this.setData({
+        isSendUrl: '../../../img/unsuccess.png',
+        isSendClick: false
+      });
+    }
+
+  }
 })
