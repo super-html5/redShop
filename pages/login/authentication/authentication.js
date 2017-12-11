8cc5bc27e997088e2ce53909fc11500320727bdd
m <chenhui315503490@126.com>
2017-12-11 18:40:09 +0800

    订单页面 和 收货地址

--- pages/login/authentication/authentication.js
+++ pages/login/authentication/authentication.js
@@ -7,7 +7,11 @@ Page({
   data: {
   
   },
-
+  toLinkAutOk: function () {
+    wx.navigateTo({
+      url: '../../login/AuthenticationOk/AuthenticationOk',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
--- pages/login/register/register.js
+++ pages/login/register/register.js
@@ -7,7 +7,11 @@ Page({
   data: {
 
   },
-
+  toLinkAut:function(){
+    wx.navigateTo({
+      url: '../../login/authentication/authentication',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
