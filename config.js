05d667e6cf5734932f7af82b10fa7a3b26249e29
mr.chen <chenhui315503490@126.com>
2017-12-22 09:12:41 +0800

    支付模块

--- config.js
+++ config.js
@@ -1,27 +1,28 @@
 //var host = 'http://192.168.1.103:3000'
 var host = 'https://xiao2.dandaojiuye.com/mall-wine'
 var config = {
-  host,
-  getTokenIdUrl: `${host}/api/v1/wine/userInfo/login`,//获取接口权限凭证
-  saveUserInfoUrl: `${host}/api/v1/wine/userInfo/auth`, //保存用户信息
-  getShoppingList: `${host}/api/v1/wine/goods/list`,//获取商品列表
-  getShoppingDetails: `${host}/api/v1/wine/goods/findById`,//根据id查询商品详情
-  getIndexTopimages: `${host}/api/v1/wine/slide/list`,//获取首页轮播图
-  getDefAddress: `${host}/api/v1/wine/address/get`,//获取用户默认地址
-  addOrder: `${host}/api/v1/wine/orders/save`,//添加订单
-  getShoppingCarList: `${host}/api/v1/wine/cart/list`,//获取购物车列表
-  addShoppingCar: `${host}/api/v1/wine/cart/save`,//添加购物车
+    host,
+    getTokenIdUrl: `${host}/api/v1/wine/userInfo/login`,//获取接口权限凭证
+    saveUserInfoUrl: `${host}/api/v1/wine/userInfo/auth`, //保存用户信息
+    getShoppingList: `${host}/api/v1/wine/goods/list`,//获取商品列表
+    getShoppingDetails: `${host}/api/v1/wine/goods/findById`,//根据id查询商品详情
+    getIndexTopimages: `${host}/api/v1/wine/slide/list`,//获取首页轮播图
+    getDefAddress: `${host}/api/v1/wine/address/get`,//获取用户默认地址
+    addOrder: `${host}/api/v1/wine/orders/save`,//添加订单
+    getShoppingCarList: `${host}/api/v1/wine/cart/list`,//获取购物车列表
+    addShoppingCar: `${host}/api/v1/wine/cart/save`,//添加购物车
 
-  getUserInfoUrl: `${host}/api/v1/wine/userInfo/getUserInfo`,//获取用户信息
-  addressListUrl: `${host}/api/v1/wine/address/list`,//获取用户地址列表
-  addressUpdateUrl: `${host}/api/v1/wine/address/update`,//添加修改删除收货地址
-  ordersListUrl: `${host}/api/v1/wine/orders/list`,//订单查询
-  cancelOrderUrl: `${host}/api/v1/wine/orders/cancel`,//订单删除
-  cartListUrl: `${host}/api/v1/wine/cart/list`,//购物车查询
-  cartClearUrl:`${host}/api/v1/wine/cart/clear`,//购物车清空
-  cartDeleteUrl: `${host}/api/v1/wine/cart/delete`,//购物车删除
-  sendMsgUrl:`${host}/api/v1/wine/common/sendMsg`,//发送验证码
-  authMsgUrl: `${host}/api/v1/wine/common/authMsg`,//验证验证码
-  paymentUrl: `${host}/api/v1/wine/wxPay/pay`, //支付
+    findOrderById: `${host}/api/v1/wine/orders/findById`,
+    getUserInfoUrl: `${host}/api/v1/wine/userInfo/getUserInfo`,//获取用户信息
+    addressListUrl: `${host}/api/v1/wine/address/list`,//获取用户地址列表
+    addressUpdateUrl: `${host}/api/v1/wine/address/update`,//添加修改删除收货地址
+    ordersListUrl: `${host}/api/v1/wine/orders/list`,//订单查询
+    cancelOrderUrl: `${host}/api/v1/wine/orders/cancel`,//订单删除
+    cartListUrl: `${host}/api/v1/wine/cart/list`,//购物车查询
+    cartClearUrl: `${host}/api/v1/wine/cart/clear`,//购物车清空
+    cartDeleteUrl: `${host}/api/v1/wine/cart/delete`,//购物车删除
+    sendMsgUrl: `${host}/api/v1/wine/common/sendMsg`,//发送验证码
+    authMsgUrl: `${host}/api/v1/wine/common/authMsg`,//验证验证码
+    paymentUrl: `${host}/api/v1/wine/wxPay/pay`, //支付
 };
 module.exports = config 
--- pages/index/orderConfirm/orderConfirm.js
+++ pages/index/orderConfirm/orderConfirm.js
@@ -1,5 +1,8 @@
 const getDefAddressUrl = require('../../../config').getDefAddress
 const addOrderUrl = require('../../../config').addOrder
+const findOrderByIdUrl = require('../../../config').findOrderById
+
+
 const utils = require('../../utils/utils');
 Page({
   /**
@@ -18,136 +21,34 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getShoppingInfo();
-  },
-  onShow: function () {
-    this.getDefAddress();
-  },
-  getShoppingInfo: function () {
-    this.init();
+    console.log(options);
+    this.init(options.id);
   },
   /**
    * 初始化
    */
-  init: function () {
-    var that = this;
-    wx.getStorage({
-      key: 'carShoppingInfo',
-      success: function (res) {
-        let _vo = JSON.parse(res.data);
-        let _nums = 0;
-        let _price = 0;
-        for (let i = 0; i < _vo.length; i++) {
-          _nums += _vo[i].number;
-          _price += _vo[i].priceTotal;
-        }
-        that.setData({
-          shoppingInfo: _vo,
-          allPrice: _price,
-          allNumber: _nums
-        });
-      }
-    })
-  },
-  /**
-   * 是否自提
-   */
-  isSend: function () {
-    if (this.data.isSendClick == 1) {
-      this.setData({
-        isSendClick: 2
-      });
-    } else {
-      this.setData({
-        isSendClick: 1
-      });
-    }
-  },
-  /**
-   * 获取用户默认地址
-   */
-  getDefAddress: function () {
+  init: function (id) {
     wx.showLoading();
     var that = this;
     wx.request({
-      url: getDefAddressUrl,
+      url: findOrderByIdUrl + '?id=' + id,
       method: 'GET',
       header: {
         'content-type': 'application/json',
         "token_id": wx.getStorageSync('token_id')
       },
       success: function (res) {
-        utils.callBackHandler(res, that.getDefAddressSuccess);
+        console.log(res);
+        that.setData({
+          shoppingInfo: res.data
+        });
       },
       fail: function (error) {
-        console.log(error)
+
       },
       complete: function () {
         wx.hideLoading();
       }
-    })
-  },
-  getDefAddressSuccess: function (res) {
-    this.setData({
-      defAddressInfo: res.data
-    })
-  },
-  /**
-   * 下订单
-   */
-  payment: function () {
-    wx.showLoading();
-    let _vo = {
-      "orders": {
-        "addressStr": this.data.defAddressInfo.location + this.data.defAddressInfo.street + this.data.defAddressInfo.detailedAddress,
-        "mobile": this.data.defAddressInfo.phone,
-        "isSelfRaised": this.data.isSendClick,
-        "useCoupon": 1,
-        "couponId": "couponId"
-      },
-      "ordersGoodsSet": []
-    }
-    let _voo = [];
-    for (let i = 0; i < this.data.shoppingInfo.length; i++) {
-      let obj = {
-        'goodsId': this.data.shoppingInfo[i].goodsId,
-        'goodsNum': this.data.shoppingInfo[i].number
-      }
-      _voo.push(obj)
-    }
-    _vo.ordersGoodsSet = _voo;
-    wx.request({
-      url: addOrderUrl,
-      header: {
-        "content-type": "application/json",
-        "token_id": wx.getStorageSync('token_id')
-      },
-      method: "POST",
-      data: _vo,
-      success: function (res) {
-        wx.removeStorage({
-          key: 'carShoppingInfo',
-          success: function (res) {
-
-          }
-        })
-        console.log(res);
-      },
-      fail: function (res) {
-        console.log(res);
-      },
-      complete: function () {
-        wx.hideLoading()
-      }
-
-    })
-  },
-  /**
-   * 更换默认地址
-   */
-  linkAddress: function () {
-    wx.navigateTo({
-      url: '/pages/user/address/address?isBack=true',
     })
   }
 })
