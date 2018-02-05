99cc290c85a25a3956318940a45ced5975e5621d
m <chenhui315503490@126.com>
2018-02-05 10:53:51 +0800

    修改  收益变化

--- pages/user/index.js
+++ pages/user/index.js
@@ -3,65 +3,81 @@
 const app = getApp()
 
 Page({
-  data: {
-    userInfo: {},
-    myIncome:''
-  },
-  //事件处理函数
-  linkOrder: function () {
-    wx.navigateTo({
-      url: '../user/order/order'
-    })
-  },
-  linkAddress: function () {
-    wx.navigateTo({
-      url: '../user/address/address'
-    })
-  },
-  linkCoupon: function () {
-    wx.navigateTo({
-      url: '../user/coupon/coupon'
-    })
-  },
-  // linkShare: function () {
-  //   wx.showToast({
-  //     title: '右上角分享哦',
-  //     icon: 'loading',
-  //     duration: 2000
-  //   })
-  // },
-  onLoad: function () {
+    data: {
+        userInfo: {},
+        myIncome: ''
+    },
+    //事件处理函数
+    linkOrder: function () {
+        wx.navigateTo({
+            url: '../user/order/order'
+        })
+    },
+    linkAddress: function () {
+        wx.navigateTo({
+            url: '../user/address/address'
+        })
+    },
+    linkCoupon: function () {
+        wx.navigateTo({
+            url: '../user/coupon/coupon'
+        })
+    },
+    // linkShare: function () {
+    //   wx.showToast({
+    //     title: '右上角分享哦',
+    //     icon: 'loading',
+    //     duration: 2000
+    //   })
+    // },
+    onLoad: function () {
         wx.getUserInfo({
-          success: res => {
-              this.setData({
-                  userInfo: res.userInfo,
-                  myIncome: app.globalData.myIncome
-              })
-          }
+            success: res => {
+                this.setData({
+                    userInfo: res.userInfo,
+                    myIncome: app.globalData.myIncome
+                })
+            }
         })
-    wx.showShareMenu({
-      withShareTicket: true
-    });
-    app.isAuth();
-  },
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    return {
-      title: '丹道小二',
-      path: '/pages/login/startUp/startUp?openid=' + app.globalData.openid,
-      imageUrl: '../../img/1501515639556_.pic_hd.jpg',
-      success: function (res) {
-        wx.showToast({
-          title: '分享成功',
-          icon: 'success',
-          duration: 2000
+        wx.showShareMenu({
+            withShareTicket: true
+        });
+        app.isAuth();
+    },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        app.getUserInfo(this.responseFun)
+    },
+    /**
+     * 回调
+     */
+    responseFun: function (res) {
+        console.log(res);
+        app.globalData.myIncome = res.data.myIncome
+        this.setData({
+            myIncome: res.data.myIncome
         })
-      },
-      fail: function (res) {
-        // 转发失败
-      }
+    },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+        return {
+            title: '丹道小二',
+            path: '/pages/login/startUp/startUp?openid=' + app.globalData.openid,
+            imageUrl: '../../img/1501515639556_.pic_hd.jpg',
+            success: function (res) {
+                wx.showToast({
+                    title: '分享成功',
+                    icon: 'success',
+                    duration: 2000
+                })
+            },
+            fail: function (res) {
+                // 转发失败
+            }
+        }
     }
-  }
 })
