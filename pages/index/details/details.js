da9d31ae450748a4ef7b33029edcb595521c7a72
mr.chen <chenhui315503490@126.com>
2018-02-05 10:06:44 +0800

    修改加载顺序

--- pages/index/details/details.js
+++ pages/index/details/details.js
@@ -19,6 +19,7 @@ Page({
     shoppingCarList: {},
     shoppingCarListNum: 0,
     vo: [],
+    images: {},
   },
 
   /**
@@ -270,6 +271,22 @@ Page({
         // 转发失败
       }
     }
+  },
+  imageLoad: function (e) {
+    var $width = e.detail.width,    //获取图片真实宽度
+      $height = e.detail.height,
+      ratio = $width / $height;    //图片的真实宽高比例
+    var viewWidth = 718,           //设置图片显示宽度，左右留有16rpx边距
+      viewHeight = 718 / ratio;    //计算的高度值
+    var image = this.data.images;
+    //将图片的datadata-index作为image对象的key,然后存储图片的宽高值
+    image[e.target.dataset.index] = {
+      width: viewWidth,
+      height: viewHeight
+    }
+    this.setData({
+      images: image
+    })
   },
   // share: function () {
   //   wx.showToast({
